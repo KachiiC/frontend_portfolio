6d090e44aa7750cf13fdd825fbd79bbc262c58fa
Kachii <Kacheokcheong@gmail.com>
2021-09-06 15:22:02 +0100

    added typscript react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+// CSS
+import './App.css';
+
+const App = (
+  <StrictMode>
+    <div className="App">
+      Kachii's Frontend Portfolio
+    </div>
+  </StrictMode>
+)
+
+export default App;
