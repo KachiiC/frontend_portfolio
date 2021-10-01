e4e2b15271f7c7247c14ac7298277a7ce201721a
Kachii <Kacheokcheong@gmail.com>
2021-10-01 01:34:33 +0100

    added images

--- src/Data/Main/Displayed/Home/HomeCardsData.tsx
+++ src/Data/Main/Displayed/Home/HomeCardsData.tsx
@@ -1,15 +1,18 @@
+import ComponentsImage from 'Images/ComponentsImage.png'
+import ProjectsImage from 'Images/ProjectsImage.png'
+
 const HomeCardsData = [
     {
         title: "Components",
         description: "A list of components designed by Kachi Cheong",
-        image: "https://via.placeholder.com/300x200/000000/FFFFFF/?text=Projects",
+        image: ComponentsImage,
         link: "components",
         link_type: "local"
     },
     {
         title: "Projects",
         description: "A list of projects designed by Kachi Cheong",
-        image: "https://via.placeholder.com/300x200/000000/FFFFFF/?text=Components",
+        image: ProjectsImage,
         link: "projects",
         link_type: "local"
     }
